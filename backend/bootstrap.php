85f948bb55cd50b4b6ba299e63e41b6259c1e4c6
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-08-30 20:34:57 +0200

    fix: correct method name load for dotenv

--- backend/bootstrap.php
+++ backend/bootstrap.php
@@ -13,8 +13,8 @@ use Dotenv\Exception\ValidationException;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/'); # load .env file
 try{
-    $dotenv->Load();
-    $dotenv->required([ 'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PORT' ])->notEmpty();
+    $dotenv->load();
+    $dotenv->required([ 'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PORT' ])->notEmpty(); 
 }
 
 catch(ValidationException $e){ // dotenv validation exception
