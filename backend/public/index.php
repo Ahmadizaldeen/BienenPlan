4788ce1b856d99671ec92ac324c14513fccf53fe
Ahmad Izaldeen <ahmad.izaldeen87@gmail.com>
2026-09-10 21:06:48 +0200

    feat: add group management routes and integrate GroupController in the Slim app

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,6 +2,8 @@
 require_once __DIR__ .'/../bootstrap.php';
 
 
+use BienenPlan\Controllers\GroupController;
+use BienenPlan\Models\Group;
 use Slim\Factory\AppFactory;
 use BienenPlan\Config\Database;
 use BienenPlan\Models\User;
@@ -25,10 +27,12 @@ try{
     //Manuelle Instanziierung der Models
     $userModel = new User($pdo);
     $taskModel = new Task($pdo);
+    $groupModel = new Group($pdo);
 
     //Manuelle Instanziierung der Controller & Middleware
     $authController = new AuthController($userModel, $jwtService);
     $taskController = new TaskController($taskModel);
+    $groupController = new GroupController($groupModel);
     $authMiddleware = new AuthMiddleware($jwtService); 
     $corsMiddleware = new CorsMiddleware();
     $apiController = new ApiController();
@@ -61,12 +65,22 @@ $app->get('/api', [$apiController, 'index']);
 $app->get('/', [$apiController, 'index']); # Setup Route
 // Geschützte Routen
 // Routen in der geschützten Gruppe registrieren
-$app->group('/api', function ($group) use ($taskController) {
-    $group->get('/tasks', [$taskController, 'getAll']);
+$app->group('/api', function ($group) use ($taskController, $groupController) {
+    $group->get('/tasks', [$taskController, 'getAllByUser']);
     $group->get('/tasks/{id}', [$taskController, 'getById']);
     $group->post('/tasks', [$taskController, 'create']);
     $group->put('/tasks/{id}', [$taskController, 'update']);
     $group->delete('/tasks/{id}', [$taskController, 'delete']);
+
+    // Gruppen-Routen
+    $group->get('/groups', [$groupController, 'getAllGroups']);
+    $group->post('/groups', [$groupController, 'createGroup']);
+    $group->post('/groups/{groupId}/addUser/{userId}', [$groupController, 'addUserToGroup']);
+    $group->get('/tasks/{taskId}/groups', [$groupController, 'getGroupsForTask']);
+    $group->get('/groups/{groupId}/users', [$groupController, 'getUsersInGroup']);
+    $group->get('/users/{userId}/groups', [$groupController, 'getGroupsForUser']);
+    $group->post('/tasks/{taskId}/assign/{groupId}', [$groupController, 'assignGroup']);
+    $group->delete('/tasks/{taskId}/groups/{groupId}', [$groupController, 'removeGroup']);
 })->add($authMiddleware);
 
 $app->run();
